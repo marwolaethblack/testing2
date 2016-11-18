dc869168caec1c9428e736dd3966e16f3c086c98
kubo45 <kozak.jakub55@gmail.com>
2016-11-18 19:01:30 +0100

    Add files via upload

---
+++ assets/scripts/admin/sortingTable.js
@@ -0,0 +1,11 @@
+﻿$(function () {
+
+    //$.tablesorter.addParser({
+    //    id: 'getDate',
+    //    is: function (sort) { return false },
+    //    format: function (sort, table, cell, cellIndex) { return $(cell).attr('data-date'); },
+    //    type:'text'
+    //})
+
+    $('table.admin-table').tablesorter();
+})
